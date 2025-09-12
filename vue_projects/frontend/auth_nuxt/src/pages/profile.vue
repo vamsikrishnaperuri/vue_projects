71ca84816a4b6fcb1e04118565a31ef5134e8de5
unknown <vamsikrishnaperuri25@gmail.com>
2025-09-12 19:44:32 +0530

    add: ui updated

--- vue_projects/frontend/auth_nuxt/src/pages/profile.vue
+++ vue_projects/frontend/auth_nuxt/src/pages/profile.vue
@@ -1,5 +1,7 @@
 <template lang="pug">
   q-layout(view="lHh Lpr lff" :style="{ background: appColor.background, minHeight: '100vh' }")
+    
+    
     appNotify(ref="notify")
 
     // Top Navigation Bar
@@ -8,113 +10,113 @@
         q-toolbar-title.text-center(:style="{color:appColor.primary}") {{ t('profile') }}
         q-btn(round dense icon="brightness_6" color="primary" @click="theme.toggleTheme")
 
-    // Left Sidebar
-    q-drawer(show-if-above bordered :style="{ background: appColor.cardbg, borderRight: '1px solid', borderColor: appColor.cardboarder }")
-      div.q-pa-md.column
-        div.flex.flex-center.column
-          q-avatar(size="100px" icon="person" :style="{ background: appColor.primary, color: appColor.background }")
-          div.text-h6.q-mt-sm(:style="{ color: appColor.text }") {{ t('account')}}
+    q-img.fit.absolute(:src="theme.mode === 'light' ? 'figma.png' : 'figma1.png'")
+      q-drawer(show-if-above bordered :style="{ background: appColor.cardbg, borderRight: '1px solid' ,  borderColor: appColor.cardboarder}")
+        div.q-pa-md.column
+          div.flex.flex-center.column
+            q-avatar(size="100px" icon="person" :style="{ background: appColor.primary, color: appColor.background }")
+            div.text-h6.q-mt-sm(:style="{ color: appColor.text }") {{ t('account')}}
 
-        q-list.q-mt-xl
-          q-item(clickable v-ripple @click="openEditDialog")
-            q-item-section(avatar)
-              q-icon(name="edit" :color="appColor.primary")
-            q-item-section
-              q-item-label(:style="{ color: appColor.text }") {{ t('editprofile')}}
+          q-list.q-mt-xl
+            q-item(clickable v-ripple @click="openEditDialog")
+              q-item-section(avatar)
+                q-icon(name="edit" :color="appColor.primary")
+              q-item-section
+                q-item-label(:style="{ color: appColor.text }") {{ t('editprofile')}}
 
-          q-separator(spaced inset :color="appColor.text")
+            q-separator(spaced inset :color="appColor.text")
 
-          q-item(clickable v-ripple @click="askconfirm")
-            q-item-section(avatar)
-              q-icon(name="delete" color="negative")
-            q-item-section
-              q-item-label(:style="{ color: appColor.text }") {{ t('deleteaccount')}}
+            q-item(clickable v-ripple @click="askconfirm")
+              q-item-section(avatar)
+                q-icon(name="delete" color="negative")
+              q-item-section
+                q-item-label(:style="{ color: appColor.text }") {{ t('deleteaccount')}}
 
-          q-separator(spaced inset :color="appColor.cardboarder")
+            q-separator(spaced inset :color="appColor.cardboarder")
 
-          q-item(clickable v-ripple @click="handleLogout")
-            q-item-section(avatar)
-              q-icon(name="logout" color="amber")
-            q-item-section
-              q-item-label(:style="{ color: appColor.text }") {{ t('logout')}}
+            q-item(clickable v-ripple @click="handleLogout")
+              q-item-section(avatar)
+                q-icon(name="logout" color="amber")
+              q-item-section
+                q-item-label(:style="{ color: appColor.text }") {{ t('logout')}}
 
-          q-separator(spaced inset :color="appColor.cardboarder")
+            q-separator(spaced inset :color="appColor.cardboarder")
 
-          //- q-item(clickable v-ripple @click="")
-          //-   q-item-section(avatar)
-          //-     q-icon(name="logout" color="amber")
-          //-   q-item-section
-          //-     q-item-label(:style="{ color: appColor.text }") {{ t('language')}}
-          //-   q-separator(spaced inset :color="appColor.cardboarder")
+            //- q-item(clickable v-ripple @click="")
+            //-   q-item-section(avatar)
+            //-     q-icon(name="logout" color="amber")
+            //-   q-item-section
+            //-     q-item-label(:style="{ color: appColor.text }") {{ t('language')}}
+            //-   q-separator(spaced inset :color="appColor.cardboarder")
 
-          div.q-pa-md(style="boarder: 2px solid #ccc")
-            q-select(
-              v-model="languageStore.current"
-              :options="[ { label: 'English', value: 'en' },{ label: 'हिन्दी', value: 'hi' },{ label: 'తెలుగు', value: 'te' }]"
-              option-value="value"
-              option-label="label"
-              emit-value
-              map-options
-              label="Language"
-              @update:model-value="languageStore.setLanguage"
-              :style="{color:appColor.secondary}"
-            )
-
-
+            div.q-pa-md(style="boarder: 2px solid #ccc")
+              q-select(
+                v-model="languageStore.current"
+                :options="[ { label: 'English', value: 'en' },{ label: 'हिन्दी', value: 'hi' },{ label: 'తెలుగు', value: 'te' }]"
+                option-value="value"
+                option-label="label"
+                emit-value
+                map-options
+                label="Language"
+                @update:model-value="languageStore.setLanguage"
+                :style="{color:appColor.secondary}"
+              )
 
 
-    // Main Profile Content - Centered
-    q-page-container
-      q-page.flex.flex-center
-        q-card(elevated :style="{width: '100%', maxWidth: '1200px', borderRadius: '20px', border: '2px solid', background: appColor.cardbg, borderColor: appColor.cardboarder}")
+
+
+      // Main Profile Content - Centered
+      q-page-container(style="background: transparent;")
+        q-page.flex.flex-center
+          q-card.q-ml-md(:style="{width: '100%', maxWidth: '1200px', borderRadius: '20px', border: '2px solid', background: appColor.cardbg, borderColor: appColor.cardboarder}")
+            q-card-section
+              div.row.q-col-gutter-md.flex.flex-center
+                // Left-aligned Profile Data (First column)
+                div.col-12.col-md-3.flex.flex-center
+                  div.q-pa-md.q-mb-md(:style="{width:'250px',border: '2px solid', borderRadius:'10px', color:appColor.text, borderColor: appColor.cardboarder}")
+                    b {{ t('name')}}: 
+                    span(:style="{color: appColor.secondary, fontWeight:'bold'}") {{ user.name }}
+                  div.q-pa-md.q-mb-md(:style="{width:'250px',border: '2px solid', borderRadius:'10px', color:appColor.text, borderColor: appColor.cardboarder}")
+                    b {{ t('lastname')}}: 
+                    span(:style="{color: appColor.secondary, fontWeight:'bold'}") {{ user.lastname }}
+                  div.q-pa-md.q-mb-md(:style="{width:'250px',border: '2px solid', borderRadius:'10px', color:appColor.text, borderColor: appColor.cardboarder}")
+                    b(:style="{color:appColor.text}") {{ t('email')}}: 
+                    span(:style="{color: appColor.secondary, fontWeight:'bold'}") {{ user.email }}
+                  div.q-pa-md.q-mb-md(:style="{width:'250px',border: '2px solid', borderRadius:'10px', color:appColor.text, borderColor: appColor.cardboarder}")
+                    b {{ t('phone')}}: 
+                    span(:style="{color: appColor.secondary, fontWeight:'bold'}") {{ user.phone }}
+                  div.q-pa-md.q-mb-md(:style="{width:'250px',border: '2px solid', borderRadius:'10px', color:appColor.text, borderColor: appColor.cardboarder}")
+                    b {{ t('gender')}}: 
+                    span(:style="{color: appColor.secondary, fontWeight:'bold'}") {{ user.gender }}
+                // Placeholder for Log Data Table (Second column)
+                div.col-12.col-md-9
+                  div.q-pa-sm(:style="{height: '100%', border: '2px solid', borderRadius:'10px', color:appColor.text, borderColor: appColor.cardboarder}")
+                    Logs
+
+      Dialog(v-model="showDialog" message="Are you sure you want to delete your account?" @confirm="handleDelete" @cancel="")
+
+      q-dialog(v-model="showEdit")
+        q-card.q-pb-sm(:style="{minWidth: '450px', borderRadius: '16px', background:appColor.cardbg, borderColor: appColor.cardboarder}")
+          q-img(src="/banner.png" alt="Profile Picture" style="height: 150px; border-bottom-left-radius: 8px; border-bottom-right-radius: 8px;")
+            div(class="absolute-full flex flex-center bg-opacity-30 text-white text-h6")
+              | Edit Profile
+
           q-card-section
-            div.row.q-col-gutter-md.flex.flex-center
-              // Left-aligned Profile Data (First column)
-              div.col-12.col-md-3.flex.flex-center
-                div.q-pa-md.q-mb-md(:style="{width:'250px',border: '2px solid', borderRadius:'10px', color:appColor.text, borderColor: appColor.cardboarder}")
-                  b {{ t('name')}}: 
-                  span(:style="{color: appColor.secondary, fontWeight:'bold'}") {{ user.name }}
-                div.q-pa-md.q-mb-md(:style="{width:'250px',border: '2px solid', borderRadius:'10px', color:appColor.text, borderColor: appColor.cardboarder}")
-                  b {{ t('lastname')}}: 
-                  span(:style="{color: appColor.secondary, fontWeight:'bold'}") {{ user.lastname }}
-                div.q-pa-md.q-mb-md(:style="{width:'250px',border: '2px solid', borderRadius:'10px', color:appColor.text, borderColor: appColor.cardboarder}")
-                  b(:style="{color:appColor.text}") {{ t('email')}}: 
-                  span(:style="{color: appColor.secondary, fontWeight:'bold'}") {{ user.email }}
-                div.q-pa-md.q-mb-md(:style="{width:'250px',border: '2px solid', borderRadius:'10px', color:appColor.text, borderColor: appColor.cardboarder}")
-                  b {{ t('phone')}}: 
-                  span(:style="{color: appColor.secondary, fontWeight:'bold'}") {{ user.phone }}
-                div.q-pa-md.q-mb-md(:style="{width:'250px',border: '2px solid', borderRadius:'10px', color:appColor.text, borderColor: appColor.cardboarder}")
-                  b {{ t('gender')}}: 
-                  span(:style="{color: appColor.secondary, fontWeight:'bold'}") {{ user.gender }}
-              // Placeholder for Log Data Table (Second column)
-              div.col-12.col-md-9
-                div.q-pa-sm(:style="{height: '100%', border: '2px solid', borderRadius:'10px', color:appColor.text, borderColor: appColor.cardboarder}")
-                  Logs
-
-    Dialog(v-model="showDialog" message="Are you sure you want to delete your account?" @confirm="handleDelete" @cancel="")
-
-    q-dialog(v-model="showEdit")
-      q-card.q-pb-sm(:style="{minWidth: '450px', borderRadius: '16px', background:appColor.cardbg, borderColor: appColor.cardboarder}")
-        q-img(src="/banner.png" alt="Profile Picture" style="height: 150px; border-bottom-left-radius: 8px; border-bottom-right-radius: 8px;")
-          div(class="absolute-full flex flex-center bg-opacity-30 text-white text-h6")
-            | Edit Profile
-
-        q-card-section
-          InputField(v-model="formData.editName" label="First Name" type="text")
-          InputField(v-model="formData.editLastName" label="Last Name" type="text")
-          InputField(v-model="formData.editEmail" label="Email" type="email")
-          InputField(v-model="formData.editphoneNumber" label="Phone Number" type="number")
-          p.q-pl-md(:style="{color:appColor.text}") Gender
-            q-option-group(
-              v-model="formData.editgender"
-              type="radio"
-              :options="[{ label: 'Male', value: 'Male' }, { label: 'Female', value: 'Female' }]"
-              color="primary"
-              inline
-            )
-        q-card-actions.q-mr-md(align="right")
-          q-btn(outline elevated label="Cancel" color="grey-7" v-close-popup)
-          q-btn(elevated label="Save" color="primary" @click="handleUpdate")
+            InputField(v-model="formData.editName" label="First Name" type="text")
+            InputField(v-model="formData.editLastName" label="Last Name" type="text")
+            InputField(v-model="formData.editEmail" label="Email" type="email")
+            InputField(v-model="formData.editphoneNumber" label="Phone Number" type="number")
+            p.q-pl-md(:style="{color:appColor.text}") Gender
+              q-option-group(
+                v-model="formData.editgender"
+                type="radio"
+                :options="[{ label: 'Male', value: 'Male' }, { label: 'Female', value: 'Female' }]"
+                color="primary"
+                inline
+              )
+          q-card-actions.q-mr-md(align="right")
+            q-btn(outline elevated label="Cancel" color="grey-7" v-close-popup)
+            q-btn(elevated label="Save" color="primary" @click="handleUpdate")
 </template>
 
 
